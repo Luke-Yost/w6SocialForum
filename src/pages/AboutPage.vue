67bffa9abbf10cba4e0d9f088d5a137f6f5bcbfa
Luke Yost <lukexyost@gmail.com>
2022-06-17 12:13:23 -0600

    added and styled about page

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="about">
-    <h1>This is the about page</h1>
+  <div class="about d-flex justify-content-center">
+    <div class="card m-3 shadow " style=" max-width: 70vw;">
+  <div class="row g-0">
+    <div class="col-md-4 p-3">
+      <img src="../assets/img/Screenshot 2022-04-25 175200.png" class="img-fluid rounded" alt="Luke Yost">
+    </div>
+    <div class="col-md-8 d-flex align-items-center">
+      <div class="card-body m-2 border border-info rounded">
+        <h2 class="card-title">About our Founder</h2>
+        <p class="card-text">We were created by Luke Yost on june 17th, 2022. Knowing how important social connections are this application was created with the intention of revolutionizing the Codeworks Social Networking system. By having a single online space where all of the Codeworks community would be able to share information not just about their professional careers, but also about their personal lives. </p>
+        
+      </div>
+    </div>
+  </div>
+</div>
   </div>
 </template>
 
