ec3c18a3abb4f51352d30f7cb93823681bcf3491
Luke Yost <lukexyost@gmail.com>
2022-06-18 01:06:19 -0600

    formatting

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="row card m-3">
+  <div class="card mb-3 p-2 bg-dark shadow">
+    <div class="row m-2 g-0 ">
+      <div class="col-md-4 d-flex ps-2 pe-4">
+        <img :src="post.imgUrl" class="img-fluid rounded align-items-center" alt="Post Image">
+      </div>
+      <div class="col-md-8 p-3  border-start border-info border-3 ">
+        <p>{{post.body}}</p>
+        <h6>- {{post.creator.name}}</h6>
+        <p>Posted {{post.createdAt}}</p>
+      </div>
+    </div>
+</div>
+</template>
+
+
+
+  <!-- <div class="row card m-3">
     <div class="col-12 d-flex justify-content-evenly">
       <div class="justify-content-start">
           <img class="postImg" :src="post.imgUrl" alt="post image">
@@ -11,18 +27,18 @@
         <img class="blog-img" :src="post.creator.picture" alt="account image">
       </div>
     </div>
-  </div>
-</template>
-
+  </div> -->
 
 <script>
   import { postsService } from "../services/PostsService"
 
+
 export default {
   props: { post: {type: Object, required: true }},
+
   setup(){
     return {
-
+    
     }
   }
 }
