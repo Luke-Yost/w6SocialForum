c96f3b2779b6367074d2e6f01c2d97bb0b3cfd07
Luke Yost <lukexyost@gmail.com>
2022-06-20 10:30:04 -0600

    formatting

--- src/pages/AccountPage.vue
+++ src/pages/AccountPage.vue
@@ -4,41 +4,41 @@
     <img class="rounded" :src="account.picture" alt="" />
     <p>{{ account.email }}</p>
   </div>
-  <form  class="row bg-dark rounded m-1" action="">
-          <div class="col-10 offset-1 m-2">
+  <form  class="row bg-dark rounded m-3 p-3 d-flex" action="">
+          <div class="col-10 offset-1 m-2 justify-content-center">
             <label for="">Account Name</label>
             <input class="form-control" type="text" v-model="accountData.name" />
           </div>
-          <div class="col-10 offset-1 m-2">
+          <div class="col-10 offset-1 m-2 justify-content-center">
             <label for="">Post Image link</label>
             <input class="form-control" type="text" v-model="accountData.picture" />
           </div>
-          <div class="col-10 offset-1 m-2">
+          <div class="col-10 offset-1 m-2 justify-content-center">
             <label for="">Bio</label>
             <input class="form-control" type="text" v-model="accountData.bio" />
           </div>
-          <div class="col-10 offset-1 m-2">
+          <div class="col-10 offset-1 m-2 justify-content-center">
             <label for="">Cover Image</label>
             <input class="form-control" type="text" v-model="accountData.coverImg" />
           </div>
-          <div class="col-10 offset-1 m-2">
+          <div class="col-10 offset-1 m-2 justify-content-center">
             <label for="">GitHub</label>
             <input class="form-control" type="text" v-model="accountData.github" />
           </div>
-          <div class="col-10 offset-1 m-2">
+          <div class="col-10 offset-1 m-2 justify-content-center">
             <label for="">Linked In</label>
             <input class="form-control" type="text" v-model="accountData.linkedin" />
           </div>
-          <div class="col-10 offset-1 m-2">
+          <div class="col-10 offset-1 m-2 justify-content-center">
             <label for="">Resume</label>
             <input class="form-control" type="text" v-model="accountData.resume" />
           </div>
-          <div class="col-10 offset-1 m-2">
+          <div class="col-10 offset-1 m-2 justify-content-center">
             <label for="">Class</label>
             <input class="form-control" type="text" v-model="accountData.class" />
           </div>
-          <div class="col-10 offset-1 m-2">
-          <button class="m-2 border border-3 border-info rounded bg-info text-light" @click.prevent="changeProfile()">Edit Profile</button>
+          <div class="col-10 offset-1 m-2 justify-content-center">
+          <button class="m-2 border border-2 border-light rounded bg-info text-light" @click.prevent="changeProfile()">Edit Profile</button>
           </div>
         </form>
 </template>
