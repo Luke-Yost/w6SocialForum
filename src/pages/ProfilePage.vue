fce491aad4919b87c8fab5a02f9b997c1ebe0eed
Luke Yost <lukexyost@gmail.com>
2022-06-19 18:09:59 -0600

    added tisments to profile page

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -6,27 +6,32 @@
         <div class="container-fluid">
           <div class="row">
             <div class="col-12 border border-4 border-info rounded bg-dark m-2">
-               <div class="container">
-  <div class="row">
-    <div class="col-md-8">
-      <div>
-        <h1>Email: {{profile.email}}</h1>
-        <h3>{{profile.bio}}</h3>
-      </div>
-      <h5>Class: {{profile.class}} | Graduated: {{profile.graduated}}</h5>
-      <h6>Github: {{profile.github}}</h6>
-      <h6>Linkedin: {{profile.linkedin}}</h6>
-      <h6>Resume: {{profile.resume}}</h6>
-    </div>
-    <div class="col-md-4">
-      <img class="img-fluid" :src="profile.picture" alt="Profile Image">
-      <h4>Name: {{profile.name}}</h4>
-    </div>
-  </div>
-  </div>
+
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-8">
+                    <div>
+                      <h3>Email: {{profile.email}}</h3>
+                      <h3>{{profile.bio}}</h3>
+                    </div>
+                    <h5>Class: {{profile.class}} | Graduated: {{profile.graduated}}</h5>
+                    <h6>Github: {{profile.github}}</h6>
+                    <h6>Linkedin: {{profile.linkedin}}</h6>
+                    <h6>Resume: {{profile.resume}}</h6>
+                  </div>
+                  <div class="col-md-4">
+                    <img class="img-fluid" :src="profile.picture" alt="Profile Image">
+                    <h4>Name: {{profile.name}}</h4>
+                  </div>
+                </div>
+              </div>
               
             </div>
           </div>
+        </div>
+
+        <div class="col-md-4">
+          <Tisment v-for="t in tisments" :tisment="t" />
         </div>
 
       </div>
@@ -39,12 +44,14 @@
 import { computed } from "vue"
 import { AppState } from "../AppState"
 import Profile  from "../components/Profile.vue"
+import Tisment from "../components/Tisment.vue"
 
 export default {
     name: 'Profile',
   setup(){
     return {
       profile: computed(()=> AppState.selectedProfile),
+      tisments: computed(() => AppState.tisments),
     }
   },
   components: { Profile }
