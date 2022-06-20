0a7e409ae09f88c255df895752e9e83524d08603
Luke Yost <lukexyost@gmail.com>
2022-06-19 22:13:51 -0600

    profile posts working

---
+++ src/components/ProfilePost.vue
@@ -0,0 +1,36 @@
+<template>
+
+    <div class="row border border-info border-3 rounded d-flex">
+      <div class="col-md-6 align-items-center">
+        <img class="img-fluid rounded " :src="profilePost?.imgUrl" alt="post image">
+      </div>
+      <div class="col-md-6">
+        <p>{{profilePost?.body}}</p>
+        <h4>likes: {{profilePost?.likes.length}}</h4>
+        <button @click="likePost(profilePost?.id)" class="btn btn-info border border-dark border-2">Like 👍</button>
+        <p class="mt-2">Posted {{(profilePost?.createdAt).substring(0,10)}}</p>
+      </div>
+    </div>
+
+</template>
+
+
+<script>
+import { postsService } from "../services/PostsService"
+
+export default {
+  props: { profilePost: { type: Object, required: true}},
+  setup(){
+    return {
+      likePost(id){
+        postsService.likePost(id)
+      }
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -11,17 +11,17 @@
                 <div class="row">
                   <div class="col-md-8">
                     <div>
-                      <h3>Email: {{profile.email}}</h3>
-                      <h3>{{profile.bio}}</h3>
+                      <h3>Email: {{profile?.email}}</h3>
+                      <h3>{{profile?.bio}}</h3>
                     </div>
-                    <h5>Class: {{profile.class}} | Graduated: {{profile.graduated}}</h5>
-                    <h6>Github: {{profile.github}}</h6>
-                    <h6>Linkedin: {{profile.linkedin}}</h6>
-                    <h6>Resume: {{profile.resume}}</h6>
+                    <h5>Class: {{profile?.class}} | Graduated: {{profile?.graduated}}</h5>
+                    <h6>Github: {{profile?.github}}</h6>
+                    <h6>Linkedin: {{profile?.linkedin}}</h6>
+                    <h6>Resume: {{profile?.resume}}</h6>
                   </div>
                   <div class="col-md-4">
-                    <img class="img-fluid" :src="profile.picture" alt="Profile Image">
-                    <h4>Name: {{profile.name}}</h4>
+                    <img class="img-fluid" :src="profile?.picture" alt="Profile Image">
+                    <h4>Name: {{profile?.name}}</h4>
                   </div>
                 </div>
               </div>
@@ -29,11 +29,16 @@
             </div>
           </div>
         </div>
-
-        <div class="col-md-4">
-          <Tisment v-for="t in tisments" :tisment="t" />
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4">
+              <Tisment v-for="t in tisments" :tisment="t" />
+            </div>
+            <div class="col-md-8">
+              <ProfilePost v-for="p in profilePosts" :key="p.id" :profilePost="p" />
+            </div>
+          </div>
         </div>
-
       </div>
     </div>
   </div>
@@ -45,16 +50,18 @@ import { computed } from "vue"
 import { AppState } from "../AppState"
 import Profile  from "../components/Profile.vue"
 import Tisment from "../components/Tisment.vue"
+import ProfilePost from "../components/ProfilePost.vue"
 
 export default {
     name: 'Profile',
   setup(){
     return {
-      profile: computed(()=> AppState.selectedProfile),
+      // profile: computed(()=> AppState.selectedProfile),
       tisments: computed(() => AppState.tisments),
+      profilePosts: computed(() => AppState.selectedProfilePosts),
     }
   },
-  components: { Profile }
+  components: { Profile,  }
 }
 </script>
 
